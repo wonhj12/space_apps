47ecdc8dc329a976f1c8fe873b1f4653fb9054d4
dayoung20 <adask13@naver.com>
2024-10-05 22:39:55 +0900

    feat:purpose추가

--- pages/purpose.py
+++ pages/purpose.py
@@ -10,6 +10,27 @@ st.title("Challenge Purpose")
 # with col2:
 #     st.write("")
 
-st.markdown("#### **배경**")
-st.write("Planetary seismology missions struggle with the power requirements necessary to send continuous seismic data back to Earth.")
-st.write
+st.markdown("### **Background**")
+# st.image("https://d2pn8kiwq2w21t.cloudfront.net/images/missionswebPIA22743-16_rfbG1OZ.2e16d0ba.fill-548x400-c50.jpg",width=300)
+
+st.markdown("""
+    <div style="border: 2px solid #001261; border-radius: 10px; padding: 20px; background-color: rgba(249, 249, 249, 0.4); margin-top: 10px;">
+        <h4 style="color: black;">Importance of Collecting Seismic Waves from Celestial Bodies</h4>
+        <p style="color: black;">Seismic waves travel at different speeds and shapes as they pass through various materials within a planet. This provides a method to study the internal structure of the planet using seismic waves.</p>
+        <p style="color: black;">Particularly in the case of Mars, it provides insights into how all rocky planets, including Earth, were formed.</p>
+        <img src="https://d2pn8kiwq2w21t.cloudfront.net/images/E1-PIA24761-Seismogram_from_Mars.width-1320.jpg" alt="Seismogram from Mars"style="width: 50%;"/>   
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style="border: 2px solid #001261; border-radius: 10px; padding: 20px; background-color: rgba(249, 249, 249, 0.4); margin-top: 30px;">
+        <h4 style="color: black;">Problem</h4>
+        <p style="color: black; font-weight:bold; font-size:18px; margin-bottom:5px">High Cost</p>
+        <p style="color: black;">1. Transmitting the vast amounts of data collected by spacecraft from celestial bodies back to Earth is very costly, as it requires a significant amount of power to send the data, which is greatly affected by distance.</p>
+        <p style="color: black;">2. Only some of this data is scientifically useful.</p>
+        <img src="images/example_image.png"/>
+    </div>
+""", unsafe_allow_html=True)
+# st.image("")
+st.markdown("### **Goal**")
+st.write("")
--- predict.py
+++ predict.py
@@ -4,12 +4,13 @@ st.set_page_config(page_title="NASA Apps", layout="wide")
 
 # 페이지 선택
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Select a page", ["purpose", "working process", "our_team"])
+page = st.sidebar.radio("Select a page", ["purpose","working process", "our team"])
 
 # 선택된 페이지에 따라 내용 표시
-if page == "Page 2":
+if page == "purpose":
     import pages.purpose
 elif page == "working process":
     import pages.working_process
 elif page == "our team":
     import pages.our_team
+
