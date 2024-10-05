015c24b843dc88f8fc79c44709a7052dd4508c49
deun115 <diddmstj15@naver.com>
2024-10-05 22:28:13 +0900

    feat: model 설명에 필요한 이미지 및 코드 추가

--- pages/working_process.py
+++ pages/working_process.py
@@ -1,3 +1,31 @@
 import streamlit as st
 
-st.title("working process")
+
+def display_model_description():
+    st.title("working process")
+
+    st.header("Model Architecture")
+    st.image("images/models/1dCNN.png", caption="1D-CNN Architecture Diagram")
+    code_string = """
+    # 1D-Model Definition
+    def create_1d_cnn_model(input_shape):
+        model = Sequential([
+            Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=input_shape),
+            MaxPooling1D(pool_size=2),
+            Conv1D(filters=128, kernel_size=3, activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Conv1D(filters=256, kernel_size=3, activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Flatten(),
+            Dense(128, activation='relu'),
+            Dropout(0.5),
+            Dense(1)  # Output Layer: Print Out Event Start Time
+        ])
+        return model
+    """
+    st.code(code_string, language="python")
+    
+    
+# 페이지가 호출될 때 함수 실행
+if __name__ == "__main__":
+    display_model_description()
