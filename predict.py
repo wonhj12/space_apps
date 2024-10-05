195105aaeb3d0fb815ffabe253c127d6d2e0ece1
ttkkwan <pollarmagic@gmail.com>
2024-10-05 20:47:33 +0900

    predict.py

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="NASA Apps", layout="wide")
 
 # 페이지 선택
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Select a page", ["Home", "Page 1", "Page 2"])
+page = st.sidebar.radio("Select a page", ["purpose", "working process", "our_team"])
 
 # 선택된 페이지에 따라 내용 표시
 if page == "Page 2":
