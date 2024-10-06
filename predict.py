49c16be9137553244dbbf2b477fb598c3f6236cb
dayoung20 <adask13@naver.com>
2024-10-06 17:32:54 +0900

    feat:purpose내용 추가

--- predict.py
+++ predict.py
@@ -7,18 +7,20 @@ import random
 
 st.set_page_config(page_title="NASA Apps", layout="wide")
 
-# 페이지 선택
-st.sidebar.title("Navigation")
-
-page = st.sidebar.radio("Select a page", ["Purpose", "Working Process", "Our Team"])
-
-# 선택된 페이지에 따라 내용 표시
-if page == "Purpose":
-    import pages.purpose
-elif page == "Working Process":
-    import pages.working_process
-elif page == "Our Team":
-    import pages.our_team
+option = st.sidebar.selectbox(
+    'Menu',
+     ('페이지1', '페이지2', '페이지3'))
+with st.sidebar:
+    choice = option_menu("Menu", ["페이지1", "페이지2", "페이지3"],
+                         icons=['house', 'kanban', 'bi bi-robot'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "4!important", "background-color": "#fafafa"},
+        "icon": {"color": "black", "font-size": "25px"},
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "—hover-color": "#fafafa"},
+        "nav-link-selected": {"background-color": "#08c7b4"},
+    }
+    )
 
 st.title('Seismic Detection')
 
